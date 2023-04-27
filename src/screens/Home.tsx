39af42800707f06d74bc61e998429d15dd16a80e
M4rcoToni <marcosanto.sl021@academico.ifsul.edu.br>
2023-04-27 13:12:29 -0300

    feat: add logout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
 import { View } from 'react-native';
+import { ButtonApp } from '../components/Controllers/Button';
+
+import { getAuth, signOut } from "firebase/auth";
+import { app } from '../firebase/firebase';
 
 export function Home() {
-  return (
-    <View className='bg-appgreen-300 flex-1'>
+  const auth = getAuth(app);
 
+  function handleLogout() {
+    signOut(auth).then(() => {
+      console.log('Logout')
+    }).catch((error) => {
+      console.log(error)
+    });
+  }
+  return (
+    <View className='bg-appwhite-200 flex-1 justify-center items-center'>
+      <ButtonApp
+        title='Sair'
+        onPress={handleLogout}
+      />
     </View>
   );
 }
